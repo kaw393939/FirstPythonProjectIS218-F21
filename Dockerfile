5574f811525e005a9f2267bd77eb81c5ac5fa2a2
kaw393939 <keith@webizly.com>
2021-10-07 13:33:19 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.9
+ADD . .
+RUN pip install -r requirements.txt
+CMD ["python", "-m", "unittest", "discover", "-s","Tests"]
---
+++ Tests/test_Calculator.py
@@ -0,0 +1,13 @@
+import unittest
+
+from Calculator.Calculator import Calculator
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+if __name__ == '__main__':
+    unittest.main()
